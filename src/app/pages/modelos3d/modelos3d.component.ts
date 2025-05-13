c039bdc4040bca4b02af11033e3bf4c60c7e4427
Andres Ojeda <andyojeda2003@gmail.com>
2025-05-13 17:19:46 -0500

    V4 (Modelo)

--- src/app/pages/modelos3d/modelos3d.component.ts
+++ src/app/pages/modelos3d/modelos3d.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { BottomNavComponent } from "../../components/bottom-nav/bottom-nav.component";
 
 @Component({
   selector: 'app-modelos3d',
-  imports: [],
+  imports: [BottomNavComponent],
   templateUrl: './modelos3d.component.html',
   styleUrl: './modelos3d.component.css',
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
